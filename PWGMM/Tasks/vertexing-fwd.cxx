b22de9a24044eeec528e25703ff354d2ec96bea6
Robin Albert Andre Caron <robin.caron@cern.ch>
2022-04-07 14:20:07 +0200

    stop dev for fwdtracks due to crash when accessing .phi()

--- PWGMM/Tasks/vertexing-fwd.cxx
+++ PWGMM/Tasks/vertexing-fwd.cxx
@@ -33,6 +33,7 @@ struct vertexingfwd {
   // Configurable<int> rangeBC{"rangeBC", 10, "Range for collision BCId and BCglobalIndex correspondance"};
   std::vector<int> vecCollForAmb;       // vector for collisions associated to an ambiguous track
   std::vector<double> vecDCACollForAmb; // vector for dca collision associated to an ambiguous track
+  std::vector<double> vecAmbTrack;      // vector for dca collision associated to an ambiguous track
 
   HistogramRegistry registry{
     "registry",
@@ -44,15 +45,17 @@ struct vertexingfwd {
      {"CollisionTime", "; Collisions time (ns); counts", {HistType::kTH1F, {{100, 0, 0.1}}}},
      {"CollisionTimeRes", "; Resolution of collision time (ns); counts", {HistType::kTH1F, {{100, 0, 0.1}}}},
      {"Chi2", "; #chi^{2}; counts", {HistType::kTH1F, {{100, 0, 10}}}},
+     {"vecCollSize", "; coll size", {HistType::kTH1F, {{100, 0, 100}}}},
      {"IndicesCollMC", "; i CollMC; i CollAmbi", {HistType::kTH2F, {{401, -0.5, 1100.5}, {401, -0.5, 1100.5}}}},
      {"NumContrib", "; N_{tr} used for the vertex; counts", {HistType::kTH1F, {{100, 0, 100}}}}} //
   };
 
-  void process(aod::AmbiguousFwdTracks const& ambitracks, aod::BCs const& bcs, soa::Join<o2::aod::FwdTracks, o2::aod::FwdTracksCov, aod::McFwdTrackLabels> const& tracks, soa::SmallGroups<soa::Join<aod::Collisions, aod::McCollisionLabels>> const& collisions, aod::McParticles const& mcParticles, aod::McCollisions const& mcCollisions) // AmbiguousMFTTracks and fwd doesn't work yet
+  void process(aod::AmbiguousMFTTracks const& ambitracks, aod::BCs const& bcs, soa::Join<o2::aod::MFTTracks, aod::McMFTTrackLabels> const& tracks, soa::Join<aod::Collisions, aod::McCollisionLabels> const& collisions, aod::McParticles const& mcParticles, aod::McCollisions const& mcCollisions) // AmbiguousMFTTracks and fwd doesn't work yet
   {
     for (auto& ambitrack : ambitracks) {
       vecCollForAmb.clear();
       vecDCACollForAmb.clear();
+      vecAmbTrack.clear();
       if (!ambitrack.bc().size()) {
         continue;
       }
@@ -73,76 +76,81 @@ struct vertexingfwd {
       }
       LOGF(info, "CI %d,  tracks.size() %d \n", ambitrack.globalIndex(), tracks.size());
       int mcCollAmbiID = -1;
-      soa::Join<o2::aod::FwdTracks, o2::aod::FwdTracksCov, aod::McFwdTrackLabels>::iterator extAmbiTrack ;
-      //auto extAmbiTrack = tracks.iteratorAt(ambitrack.globalIndex());
-      auto extAmbiTrackid = ambitrack.fwdtrackId();
-        for (auto& track : tracks) {
-            LOGF(info, "  ---- >> %d  -----  %d \n", extAmbiTrackid, track.globalIndex() );
-            if(extAmbiTrackid == track.globalIndex() ){
-                LOGF(info, "  -----  %d \n", track.globalIndex() );
-                  if (!track.has_mcParticle()) {
-                    LOGF(warning, "No MC particle for ambiguous track, skip...");
-                    continue;
-                  }
-                auto particle = track.mcParticle();
-                mcCollAmbiID = particle.mcCollisionId();
-                LOGF(info, "mcCollAmbiID %d \n", mcCollAmbiID);
-                extAmbiTrack = track;
-                break;
-            }
+      // soa::Join<o2::aod::FwdTracks, o2::aod::FwdTracksCov, aod::McFwdTrackLabels>::iterator extAmbiTrack;
+      soa::Join<o2::aod::MFTTracks, aod::McMFTTrackLabels>::iterator extAmbiTrack;
+
+      // auto extAmbiTrack = tracks.iteratorAt(ambitrack.globalIndex());
+      auto extAmbiTrackid = ambitrack.mfttrackId();
+      bool itIsDef = false;
+      for (auto& track : tracks) {
+        if (extAmbiTrackid == track.globalIndex()) {
+          if (!track.has_mcParticle()) {
+            LOGF(warning, "No MC particle for ambiguous track, skip...");
+            continue;
+          }
+          auto particle = track.mcParticle();
+          mcCollAmbiID = particle.mcCollisionId();
+          vecAmbTrack.push_back(track.x());
+          vecAmbTrack.push_back(track.y());
+          vecAmbTrack.push_back(track.phi());
+          vecAmbTrack.push_back(track.tgl());
+          vecAmbTrack.push_back(track.signed1Pt());
+          vecAmbTrack.push_back(track.z());
+          vecAmbTrack.push_back(track.chi2());
+
+          extAmbiTrack = track; // can not access to the extAmbiTrack .x, .y(), .phi() ...
+          itIsDef = true;
+          break;
         }
-        //if( track.globalIndex() == (tracks.size()-1) ) continue;
-//      if (!extAmbiTrack.phi()) {
-//        continue;
-//      }
+      }
+      // if( track.globalIndex() == (tracks.size()-1) ) continue;
+      if (!extAmbiTrack.x()) {
+        continue;
+      }
       // printf("extAmbiTrack %d \n", ambitrack.globalIndex());
-      //LOGF(info, "extAmbiTrack %d \n", extAmbiTrack.phi());
+      // LOGF(info, "extAmbiTrack %d \n", extAmbiTrack.phi());
       //      if (!extAmbiTrack.has_mcParticle()) {
       //        LOGF(warning, "No MC particle for ambiguous track, skip...");
       //        continue;
       //      }
-//      auto particle = extAmbiTrack.mcParticle();
-//      int mcCollAmbiID = particle.mcCollisionId();
+      //      auto particle = extAmbiTrack.mcParticle();
+      //      int mcCollAmbiID = particle.mcCollisionId();
 
       // printf("phi = %f, mcCollAmbiID  %d \n", extAmbiTrack.phi(), mcCollAmbiID);
 
       for (auto& bc : ambitrack.bc()) {
-        // LOGF(info, "  BC %d with global BC %lld", bc.globalIndex(), bc.globalBC());
+        // LOGF(info, "       5) BC %d with global BC %lld", bc.globalIndex(), bc.globalBC());
 
         for (auto& collision : collisions) {
-          int rangeBC = 3; // retrive the global bc for the indices given in the bcslice vector
+          int rangeBC = 100; // retrive the global bc for the indices given in the bcslice vector
           registry.fill(HIST("EventSelection"), 1.);
 
-          // printf("collision BC ID = %lld, bc.globalIndex() %lld\n", collision.bcId(), bc.globalIndex());
-          if ((bcs.iteratorAt(collision.bcId()).globalBC() > (bc.globalBC() - rangeBC)) && (bcs.iteratorAt(collision.bcId()).globalBC() < (bc.globalBC() + rangeBC))) {
-            registry.fill(HIST("EventsNtrkZvtx"), tracks.size(), collision.posZ());
-            registry.fill(HIST("EventSelection"), 2.);
-            // printf("collision BC ID = %d, bc.globalIndex() %lld\n", collision.bcId(), bc.globalIndex());
-            // printf("collision pos Z %f\n", collision.posZ());
+          if ((collision.bc().globalBC() > (bc.globalBC() - rangeBC)) && (collision.bc().globalBC() < (bc.globalBC() + rangeBC))) {
+            // registry.fill(HIST("EventsNtrkZvtx"), tracks.size(), collision.posZ());
+            // registry.fill(HIST("EventSelection"), 2.);
+            //  printf("collision BC ID = %d, bc.globalIndex() %lld\n", collision.bcId(), bc.globalIndex());
+            // LOGF(info,"collision pos Z %f\n", collision.posZ());
 
-            SMatrix5 tpars(extAmbiTrack.x(), extAmbiTrack.y(), extAmbiTrack.phi(), extAmbiTrack.tgl(), extAmbiTrack.signed1Pt());
+            SMatrix5 tpars(vecAmbTrack[0], vecAmbTrack[1], vecAmbTrack[2], vecAmbTrack[3], vecAmbTrack[4]);
             // printf("SMatrix5  extAmbiTrack.signed1Pt() %f\n", extAmbiTrack.signed1Pt());
-
-//            std::vector<double> v1{extAmbiTrack.cXX(), extAmbiTrack.cXY(), extAmbiTrack.cYY(), extAmbiTrack.cPhiX(), extAmbiTrack.cPhiY(),
-//                                   extAmbiTrack.cPhiPhi(), extAmbiTrack.cTglX(), extAmbiTrack.cTglY(), extAmbiTrack.cTglPhi(), extAmbiTrack.cTglTgl(),
-//                                   extAmbiTrack.c1PtX(), extAmbiTrack.c1PtY(), extAmbiTrack.c1PtPhi(), extAmbiTrack.c1PtTgl(), extAmbiTrack.c1Pt21Pt2()};
+            //            std::vector<double> v1{extAmbiTrack.cXX(), extAmbiTrack.cXY(), extAmbiTrack.cYY(), extAmbiTrack.cPhiX(), extAmbiTrack.cPhiY(),
+            //                                   extAmbiTrack.cPhiPhi(), extAmbiTrack.cTglX(), extAmbiTrack.cTglY(), extAmbiTrack.cTglPhi(), extAmbiTrack.cTglTgl(),
+            //                                   extAmbiTrack.c1PtX(), extAmbiTrack.c1PtY(), extAmbiTrack.c1PtPhi(), extAmbiTrack.c1PtTgl(), extAmbiTrack.c1Pt21Pt2()};
             std::vector<double> v1;
             // printf("vector<double>  extAmbiTrack.cXX() %f\n", extAmbiTrack.cXX());
 
             SMatrix55 tcovs(v1.begin(), v1.end());
-            o2::track::TrackParCovFwd pars1{extAmbiTrack.z(), tpars, tcovs, extAmbiTrack.chi2()};
+
+            o2::track::TrackParCovFwd pars1{vecAmbTrack[5], tpars, tcovs, vecAmbTrack[6]};
             // double chi2 = 1.0;
             // o2::track::TrackParCovFwd pars1{extAmbiTrack.z(), tpars, tcovs, chi2};
             // printf("SMatrix55  extAmbiTrack.chi2() %f\n", extAmbiTrack.chi2());
-
             pars1.propagateToZlinear(collision.posZ());
             // printf("propagateToZlinear  collision.posZ() %f\n", collision.posZ());
-
             const auto dcaX(pars1.getX() - collision.posX());
             const auto dcaY(pars1.getY() - collision.posY());
             auto dcaXY = std::sqrt(dcaX * dcaX + dcaY * dcaY);
             // printf("dcaXY  %f\n", dcaXY);
-
             registry.fill(HIST("TrackDCAxy"), dcaXY);
             registry.fill(HIST("TrackDCAx"), dcaX);
             registry.fill(HIST("TrackDCAy"), dcaY);
